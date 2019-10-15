5913fdc37aede606f7d1737fec8fc2fd2cd5b747
mayel <m.ricou@rubika-edu.com>
2019-10-15 17:52:48 +0200

    PlayerController 1/3
    Base de PlayerController done, extrajump marche pas, mauvais Gamefeel, gravité de merde

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    public Transform groundCheck;
+    private bool isGrounded;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+    private void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    private void Update()
+    {
+
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        {
+            if ((isGrounded == false))
+            {
+                rb.velocity = new Vector2(); 
+                if (Input.GetKeyDown("up"))
+                {
+                    rb.velocity = new Vector2();
+                }
+            }
+
+            if ((isGrounded == false))
+            {
+                rb.velocity = new Vector2(); 
+                if (Input.GetKeyDown("up"))
+                {
+                    rb.velocity = new Vector2();
+                }
+            }
+        }
+    }
+}
